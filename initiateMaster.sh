91f3400bc0b8bc530a2035e89f1823b19b531a79
TobiasAndersson <anderssontobias93@gmail.com>
2016-10-21 13:13:19 +0200

    Update initiateMaster.sh

--- initiateMaster.sh
+++ initiateMaster.sh
@@ -8,9 +8,3 @@ while ! ping -c1 $FLOATING_IP &>/dev/null; do echo $FLOATING_IP; done
 sleep 10
 scp -o "StrictHostKeyChecking no" -i group12Key.key floating_ip.txt ubuntu@$FLOATING_IP:/home/ubuntu/
 ssh -o "StrictHostKeyChecking no" -i group12Key.key ubuntu@$FLOATING_IP 'bash -s' < setupscriptMaster.sh
-#curl -i http://$FLOATING_IP:5000/ -o result.txt --verbose
-#sleep 5
-#nova delete none_VM_proj_group12_1
-#sleep 10
-#echo "Deleting VM"
-#nova delete none_VM_proj_group12_1
